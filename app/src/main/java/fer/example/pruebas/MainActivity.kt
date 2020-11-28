8d55be4d1964c38c78cd66000e2baadc63ee2eab
fernanda-rocha <fernanda.lennon@comunidad.unam.mx>
2020-11-28 13:31:30 -0600

    Initial commit

---
+++ app/src/main/java/fer/example/pruebas/MainActivity.kt
@@ -0,0 +1,42 @@
+package fer.example.pruebas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bSaludar = findViewById<Button>(R.id.bSaludar)
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val cbDev = findViewById<CheckBox>(R.id.cbDeveloper)
+        bSaludar.setOnClickListener(View.OnClickListener {
+
+            if(validaDato()){
+                if (cbDev.isChecked){
+                    Toast.makeText(this, "Bienvenido" + etNombre.text + ", sé que eres desarrollador", Toast.LENGTH_LONG).show()
+                }else {
+                    Toast.makeText(this, "Bienvenido" + etNombre.text, Toast.LENGTH_LONG).show()
+                }
+            }else{
+                Toast.makeText(this, "Escribe tu nombre para saludarte :(", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+    fun validaDato(): Boolean{
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val nombreUsuario = etNombre.text
+
+        if(nombreUsuario.isNullOrEmpty()){
+            return false
+        }
+        return true
+    }
+}
